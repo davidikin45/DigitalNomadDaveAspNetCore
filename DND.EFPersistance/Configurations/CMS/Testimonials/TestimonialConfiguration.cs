f51bff2d3e61423ad64a536d7bf23b58e3f33638
Digital Nomad Dave <davidikin45@gmail.com>
2018-04-25 20:24:30 +1000

    Optimistic Concurrency

--- DND.EFPersistance/Configurations/CMS/Testimonials/TestimonialConfiguration.cs
+++ DND.EFPersistance/Configurations/CMS/Testimonials/TestimonialConfiguration.cs
@@ -11,7 +11,7 @@ namespace DND.EFPersistance.Configurations.CMS.Testimonials
         {
             HasKey(p => p.Id);
 
-            Property(p => p.RowVersion).IsRowVersion(); // Only works for byte array.
+            Property(p => p.RowVersion).IsRowVersion(); // Only works for byte array. Will create a timestamp in EF6, rowversion EFCore
             //Property(p => p.RowVersion).IsConcurrencyToken().HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Computed); //works for int
 
             Property(p => p.Source)
