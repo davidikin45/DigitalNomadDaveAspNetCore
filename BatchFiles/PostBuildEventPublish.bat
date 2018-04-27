2e932a9936a43c2873d109968740e4cd46e87141
Digital Nomad Dave <davidikin45@gmail.com>
2018-04-27 18:53:25 +1000

    Changes

--- BatchFiles/PostBuildEventPublish.bat
+++ BatchFiles/PostBuildEventPublish.bat
@@ -1,2 +1,7 @@
-xcopy "%1net47\plugins\*.dll" "%2plugins\" /Y
-xcopy "%1net47\plugins\*.pdb" "%2plugins\" /Y
+Echo.%2 | findstr /C:":">nul && (
+    xcopy "%1net47\plugins\*.dll" "%2plugins\" /Y
+    xcopy "%1net47\plugins\*.pdb" "%2plugins\" /Y
+) || (
+    xcopy "%1net47\plugins\*.dll" "%3plugins\" /Y
+    xcopy "%1net47\plugins\*.pdb" "%3plugins\" /Y
+)
