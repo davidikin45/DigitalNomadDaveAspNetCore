b2e3c9e1038311597e5b2142f9f698dec207ee8c
Digital Nomad Dave <davidikin45@gmail.com>
2018-05-15 18:54:16 +1000

    net472 changes

---
+++ BatchFiles/CleanBinFolders.bat
@@ -0,0 +1,61 @@
+set folder="..\src\DND.Web\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.Bot\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.Common\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.Domain\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.ApplicationServices\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.DomainServices\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.EFPersistance\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.EFPersistance.Identity\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.Infrastructure\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.Repository\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.UnitOfWork\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\Marvin.Cache.Headers\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\..\tests\DND.UnitTests\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.IntegrationTests\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+set folder="..\..\DND.IntegrationTests2\bin"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+
