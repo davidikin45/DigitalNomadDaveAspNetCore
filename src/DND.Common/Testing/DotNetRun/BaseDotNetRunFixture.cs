d8f7d1e83163d7f34c00f6c98de7d8bb9cd84011
Digital Nomad Dave <davidikin45@gmail.com>
2018-05-19 13:07:04 +1000

    Process Changes

--- src/DND.Common/Testing/DotNetRun/BaseDotNetRunFixture.cs
+++ src/DND.Common/Testing/DotNetRun/BaseDotNetRunFixture.cs
@@ -1,14 +1,16 @@
 ﻿using DND.Common.Extensions;
+using DND.Common.Processes;
 using Microsoft.Extensions.PlatformAbstractions;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace DND.Common.Testing.DotNetRun
 {
     public abstract class BaseDotNetRunFixture : IDisposable
     {
-        private Process _process;
+        private SimpleProcess _process;
         private string _environment;
         private string _webAppRelativePath;
 
@@ -21,40 +23,19 @@ namespace DND.Common.Testing.DotNetRun
             _environment = environment;
             _hideBrowser = hideBrowser;
             _url = url;
+
+            string filename = "dotnet.exe";
+            string args = $@"run --project {GetContentRootPath()}\ environment={_environment} --no-build --urls ""{_url}""";
+
+            _process = new SimpleProcess(filename, args, _hideBrowser);
         }
 
         public void Launch()
         {
             if (!string.IsNullOrEmpty(_webAppRelativePath) && !string.IsNullOrEmpty(_environment) && _url.Contains("localhost"))
             {
-                StartServer();
+                _process.StartProcess();
             }
-        }
-
-        private void StartServer()
-        {
-            _process = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "dotnet",
-                    Arguments = $@"run --project {GetContentRootPath()}\ environment={_environment} --no-build --urls ""{_url}"""
-                }
-            };
-
-            if (_hideBrowser)
-            { 
-                _process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                _process.StartInfo.CreateNoWindow = true;
-                _process.StartInfo.UseShellExecute = false;
-            }
-
-            _process.Start();
-        }
-
-        private string GetProjectFile()
-        {
-            return Path.GetFileName(_webAppRelativePath) + ".csproj";
         }
 
         private string GetContentRootPath()
@@ -64,21 +45,17 @@ namespace DND.Common.Testing.DotNetRun
             return contentPath;
         }
 
-        public bool ProcessRunning
+        public bool IsRunning
         {
             get
             {
-                return !_process.HasExited;
+                return _process.IsRunning;
             }
         }
 
         public void Dispose()
         {
-            if(_process != null)
-            {
-                _process.KillProcessAndChildren();
-            }
-           
+            _process.Dispose();
         }
     }
 }
