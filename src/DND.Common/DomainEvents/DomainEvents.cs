6f6ac31c11454fc6c2eb5b7ebbbd54abae5561b5
Digital Nomad Dave <davidikin45@gmail.com>
2018-05-21 22:05:56 +1000

    Domain Events

--- src/DND.Common/DomainEvents/DomainEvents.cs
+++ src/DND.Common/DomainEvents/DomainEvents.cs
@@ -1,8 +1,11 @@
 ﻿using DND.Common.DomainEvents;
 using DND.Common.Infrastructure;
+using DND.Common.Interfaces.UnitOfWork;
+using Hangfire;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +47,7 @@ namespace DND.Common.DomainEvents
                 .ToList();
         }
 
+        //InProcess
         public async static Task DispatchPreCommitAsync(IDomainEvent domainEvent)
         {
             if(_handlers != null)
@@ -58,12 +62,13 @@ namespace DND.Common.DomainEvents
                     if (canHandleEvent)
                     {
                         dynamic handler = StaticProperties.HttpContextAccessor.HttpContext.RequestServices.GetService(handlerType);
-                        await handler.HandlePreCommit((dynamic)domainEvent);
+                        await handler.HandlePreCommitAsync((dynamic)domainEvent);
                     }
                 }
             }
         }
 
+        //InProcess at the moment but hopefully can make it out of process
         public async static Task DispatchPostCommitAsync(IDomainEvent domainEvent)
         {
             if (_handlers != null)
@@ -78,10 +83,29 @@ namespace DND.Common.DomainEvents
                     if (canHandleEvent)
                     {
                         dynamic handler = StaticProperties.HttpContextAccessor.HttpContext.RequestServices.GetService(handlerType);
-                        await handler.HandlePostCommit((dynamic)domainEvent);
+
+                        //Would be good to send this off to hangfire as a background job.
+                        //Hangfire unfortunately uses System.Type.GetType to get job type. This only looks at the referenced assemblies of the web project and not the dynamic loaded plugins :(
+                        //var exp = GetExpression(handlerType, domainEvent);
+
+                        //MethodInfo method = typeof(BackgroundJob).GetMethods().Where(m => m.Name == "Enqueue").Last();
+                        //MethodInfo generic = method.MakeGenericMethod(handlerType);
+                        //generic.Invoke(null, new object[] { exp });
+
+                        await handler.HandlePostCommitAsync((dynamic)domainEvent);
                     }
                 }
             }
+        }
+
+        private static LambdaExpression GetExpression(Type handlerType, object domainEvent)
+        {
+            var parameterExp = Expression.Parameter(handlerType, "type");
+            MethodInfo method = handlerType.GetMethod("HandlePostCommitAsync", BindingFlags.Instance | BindingFlags.Public);
+            var someValue = Expression.Constant(domainEvent, domainEvent.GetType());
+            var handlePostCommitAsyncExp = Expression.Call(parameterExp, method, someValue);
+
+            return Expression.Lambda(handlePostCommitAsyncExp, parameterExp);
         }
     }
 }
