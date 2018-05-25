682c29bc6c6c0756c43e26bb59e1232f88b40837
Digital Nomad Dave <davidikin45@gmail.com>
2018-05-25 11:42:38 +1000

    Bug

--- src/DND.Common/Implementation/UnitOfWork/Scopes/UnitOfWorkTransactionalScope.cs
+++ src/DND.Common/Implementation/UnitOfWork/Scopes/UnitOfWorkTransactionalScope.cs
@@ -5,7 +5,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
-using System
+using System;
 using System.Collections;
 using System.Data;
 using System.Data.Entity;
