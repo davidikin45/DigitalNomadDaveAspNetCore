17f562c59bc215f5d85d70988ce551b1cac29b7d
Digital Nomad Dave <davidikin45@gmail.com>
2018-08-03 19:26:30 +0300

    Cookies

---
+++ src/DND.Common/Implementation/Services/CookieService.cs
@@ -0,0 +1,43 @@
+﻿using DND.Common.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND.Common.Implementation.Services
+{
+    public class CookieService : ICookieService
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        public CookieService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string Get(string key)
+        {
+            if (_httpContextAccessor.HttpContext.Request.Cookies.ContainsKey(key))
+                return _httpContextAccessor.HttpContext.Request.Cookies[key];
+
+            return "";
+        }
+
+        public void Remove(string key)
+        {
+           _httpContextAccessor.HttpContext.Response.Cookies.Delete(key);
+        }
+
+        public void Set(string key, string value, int? expireDays)
+        {
+            CookieOptions option = new CookieOptions();
+            if (expireDays.HasValue)
+                option.Expires = DateTime.Now.AddDays(expireDays.Value);
+            else
+                option.Expires = DateTime.Now.AddMilliseconds(10);
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Append(key, value, option);
+        }
+    }
+}
---
+++ src/DND.Common/Interfaces/Services/ICookieService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND.Common.Interfaces.Services
+{
+    public interface ICookieService
+    {
+        string Get(string key);
+        void Set(string key, string value, int? expireTime);
+        void Remove(string key);
+    }
+}
