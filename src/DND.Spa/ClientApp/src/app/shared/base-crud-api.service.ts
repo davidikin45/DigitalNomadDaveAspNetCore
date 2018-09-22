6eed4a25ad5da395c41b1325943504fd08806ac7
David Roy Ikin <davidikin45@gmail.com>
2018-09-22 22:43:14 +0300

    Angular fix

--- src/DND.Spa/ClientApp/src/app/shared/base-crud-api.service.ts
+++ src/DND.Spa/ClientApp/src/app/shared/base-crud-api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, tap, filter, catchError, mergeMap } from "rxjs/operators";
-import { environment } from '../../environments/environment';
 import { Operation } from 'fast-json-patch';
 import { BaseService } from './base.service';
 
