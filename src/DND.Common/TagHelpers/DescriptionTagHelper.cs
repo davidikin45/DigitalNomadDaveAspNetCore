d285b953a57e0d0e8d7afc81362a8f6d1bf9887a
David Ikin <davidikin45@gmail.com>
2018-09-27 16:21:00 +0300

    Description Tag Helper

---
+++ src/DND.Common/TagHelpers/DescriptionTagHelper.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND.Common.TagHelpers
+{
+    [HtmlTargetElement("label", Attributes = DescriptionForAttributeName)]
+    public class LabelDescriptionTagHelper : TagHelper
+    {
+        private const string DescriptionForAttributeName = "asp-description-for";
+
+        public LabelDescriptionTagHelper(IHtmlGenerator generator)
+        {
+            Generator = generator;
+        }
+
+        /// <inheritdoc />
+        public override int Order
+        {
+            get
+            {
+                return -1000;
+            }
+        }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        protected IHtmlGenerator Generator { get; }
+
+        [HtmlAttributeName(DescriptionForAttributeName)]
+        public ModelExpression DescriptionFor { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var metadata = DescriptionFor.Metadata;
+
+            if (metadata == null)
+            {
+                throw new InvalidOperationException(string.Format("No provided metadata ({0})", DescriptionForAttributeName));
+            }
+
+            output.Attributes.SetAttribute("id", metadata.PropertyName + "-description");
+            output.Attributes.SetAttribute("for", metadata.PropertyName);
+
+            if (!string.IsNullOrWhiteSpace(metadata.Description))
+            {
+                output.Content.SetContent(metadata.Description);
+            }
+        }
+    }
+}
