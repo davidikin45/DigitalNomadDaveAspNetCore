7247c9682499b90588fa8e0ff308250e22c23c28
David Ikin <davidikin45@gmail.com>
2018-09-30 19:03:48 +0300

    Multiline text

--- src/DND.Web/Mvc/Contact/Models/ContactViewModel.cs
+++ src/DND.Web/Mvc/Contact/Models/ContactViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DND.Common.Domain.ModelMetadata;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -21,6 +22,7 @@ namespace DND.Web.Mvc.Contact.Models
         public string Subject { get; set; }
 
         [Required]
+        [MultilineText]
         public string Message { get; set; }
     }
 }
